cb34dd33a8ed23960aed049a8159c3c2a46e0809
seveibar <seveibar@gmail.com>
2023-10-29 21:22:52 -0700

    transform working

--- tests/playground/transform-tsx.test.ts
+++ tests/playground/transform-tsx.test.ts
@@ -1,3 +1,21 @@
 import { transform } from "@babel/standalone"
+import reactJsx from "@babel/plugin-transform-react-jsx"
 
-test("should transform tsx to browser compatible code", () => {})
+test("should transform tsx to browser compatible code", () => {
+  const result = transform(
+    `
+  
+export const App = () => (
+  <div>
+    hello world!
+  </div>
+)
+
+`.trim(),
+    {
+      plugins: ["transform-react-jsx"],
+    }
+  )
+
+  expect(result.code).toBe(0)
+})
