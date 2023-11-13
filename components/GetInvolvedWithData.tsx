368b4c8f882759bf2ac91b3f6d20b6661e3691a9
seveibar <seveibar@gmail.com>
2023-11-12 21:34:51 -0800

    force "get involved" to revalidate cache every hour

--- components/GetInvolvedWithData.tsx
+++ components/GetInvolvedWithData.tsx
@@ -2,7 +2,12 @@ import { GetInvolved } from "./GetInvolved"
 
 export const GetInvolvedWithData = async () => {
   const overview = await fetch(
-    "https://changelog.tscircuit.com/changelogs/overview.json"
+    "https://changelog.tscircuit.com/changelogs/overview.json",
+    {
+      next: {
+        revalidate: 3600,
+      },
+    }
   ).then((r) => r.json())
 
   return <GetInvolved overview={overview} />
