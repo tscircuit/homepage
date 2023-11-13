707106acf58c49b1330875d4617d1084a93a9d58
seveibar <seveibar@gmail.com>
2023-11-12 21:34:30 -0800

    force changelog to revalidate data every hour

--- components/ChangelogWithData.tsx
+++ components/ChangelogWithData.tsx
@@ -2,7 +2,12 @@ import { Changelog } from "./Changelog"
 
 export const ChangelogWithData = async () => {
   const changelog = await fetch(
-    "https://changelog.tscircuit.com/changelogs/overview.json"
+    "https://changelog.tscircuit.com/changelogs/overview.json",
+    {
+      next: {
+        revalidate: 3600,
+      },
+    }
   ).then((r) => r.json())
 
   return <Changelog changelog={changelog} />
